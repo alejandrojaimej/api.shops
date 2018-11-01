8700a9fd70ae03fcdfa1a163c61761edcb11bcb5
alejandrojaimej <alejandro@mk1.es>
2018-11-01 03:02:27 +0100

    viendo el usuario leido de la base de datos

--- models/Users.model.php
+++ models/Users.model.php
@@ -16,6 +16,7 @@ class Users extends Model{
     $stm = self::$db->prepare($query);
     $stm->execute(array('email'=>$email));
     $user = $stm->fetchAll();
+    return $user;
     if( password_verify($password, $user['password']) ){
       return $user['id'];  
     }else{
