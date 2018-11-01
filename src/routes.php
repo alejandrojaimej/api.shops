7821777d7695b4a8d4f3f58a2647a65309631f9a
alejandrojaimej <alejandro@mk1.es>
2018-11-01 01:18:47 +0100

    eliminadas dos funciones sobrantes de la api

--- src/routes.php
+++ src/routes.php
@@ -5,42 +5,6 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/{lang}', function(Request $request, Response $response, array $args){
-    $resp = auth($request, $response);if($resp != 'valid'){return $resp;}
-    $shell = loadModel('Shell');
-
-    $result = [];
-    $result['main-image'] = $shell::getMainImage();
-    $result['main-searcher'] = $shell::getMainSearcher(false, $args['lang']);
-    
-
-    $response_data = array();
-    $response_data['error'] = false; 
-    $response_data['response'] = $result; 
-    $response->write(json_encode($response_data));
-
-    return $response
-    ->withHeader('Content-type', 'application/json')
-    ->withStatus(200);  
-});
-
-$app->get('/searchLocation/{lang}/{string}', function(Request $request, Response $response, array $args){
-    $resp = auth($request, $response);if($resp != 'valid'){return $resp;}
-    $locations = loadModel('Locations');
-
-    $result = [];
-    $result['search-result'] = $locations::searchByString($args['lang'], $args['string']);
-    
-    $response_data = array();
-    $response_data['error'] = false; 
-    $response_data['response'] = $result; 
-    $response->write(json_encode($response_data));
-
-    return $response
-    ->withHeader('Content-type', 'application/json')
-    ->withStatus(200);  
-});
-
 /**
  * Devuelve todos los textos para una view determinada
  * @param lang Tiene que ser uno de los definidos en LANGS (globlas.php) en caso de ser otro devuelve el primer idioma definido.
