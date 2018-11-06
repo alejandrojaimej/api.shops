5d308c393e74ea0be6b479ce95ed16e122a2c666
reyes jimenez <reyesjimenez@MacBook-Pro-de-reyes.local>
2018-11-06 17:55:50 +0100

    probando activateUser

--- src/routes.php
+++ src/routes.php
@@ -70,7 +70,10 @@ $app->get('/activateUser/{userToken}', function(Request $request, Response $resp
     $resp = auth($request, $response);if($resp != 'valid'){return $resp;}
     $users = loadModel('Users');
 
-    $result = $users::activateUser(urldecode($args['userToken']));
+    echo '1: '.urldecode($args['userToken']);
+    echo '<br>2: '.urldecode($request->getParam('userToken'));
+
+    /*$result = $users::activateUser(urldecode($args['userToken']));
 
     $response_data = array();
     $response_data['error'] = false; 
@@ -79,5 +82,5 @@ $app->get('/activateUser/{userToken}', function(Request $request, Response $resp
 
     return $response
     ->withHeader('Content-type', 'application/json')
-    ->withStatus(200);  
+    ->withStatus(200);  /*
 });
