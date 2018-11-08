a84c02587e5e4fad196b2f73b5f43e0f06555c18
reyes jimenez <reyesjimenez@MacBook-Pro-de-reyes.local>
2018-11-08 13:01:20 +0100

    Añadida función para obtener todos los textos de la administración

--- models/Shell.model.php
+++ models/Shell.model.php
@@ -16,5 +16,31 @@ class Shell extends Model{
     $stm->execute(array('controller'=>$controller));
     return $stm->fetchAll(PDO::FETCH_KEY_PAIR);
   }
+
+  /**
+   * Devuelve los textos para el buscador principal
+   * @param id: Devuelve texto específico dado un Id (podría hacerse por "component"..al gusto)
+   * @param lang: Idioma de los textos
+   */
+  public static function getAdminText($lang = LANGS[0], $controller = 'login', $userId = false){
+    if($userId === false){ return false; }
+    $search = 'component, '.(!in_array($lang, LANGS) ? LANGS[0] : $lang);
+    $query = 'SELECT '.$search.' FROM translations WHERE controller=:controller OR controller="base"';
+    $stm = self::$db->prepare($query);
+    $stm->execute(array('controller'=>$controller));
+    $textos = $stm->fetchAll(PDO::FETCH_KEY_PAIR);
+
+    $query = 'SELECT id, name FROM commerces WHERE userId = :user';
+    $stm = self::$db->prepare($query);
+    $stm->execute(array('userId'=>$userId));
+    $comercios = $stm->fetchAll(PDO::FETCH_KEY_PAIR);
+
+    $query = 'SELECT id, name, surname FROM user_details WHERE userId = :user';
+    $stm = self::$db->prepare($query);
+    $stm->execute(array('userId'=>$userId));
+    $usuario = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    return array('texts'=>$textos, 'comerces'=>$comercios, 'user'=>$usuario);
+  }
 }
 ?>
--- src/routes.php
+++ src/routes.php
@@ -26,6 +26,26 @@ $app->get('/text/{lang}/{controller}', function(Request $request, Response $resp
 });
 
 /**
+ * Devuelve todos los textos para la administración
+ * @param lang Tiene que ser uno de los definidos en LANGS (globlas.php) en caso de ser otro devuelve el primer idioma definido.
+ */
+$app->get('/adminText/{lang}/{controller}/{userId}', function(Request $request, Response $response, array $args){
+    $resp = auth($request, $response);if($resp != 'valid'){return $resp;}
+    $shell = loadModel('Shell');
+
+    $result = $shell::getAdminText($args['lang'], $args['controller'], $args['userId']);
+
+    $response_data = array();
+    $response_data['error'] = false; 
+    $response_data['response'] = $result; 
+    $response->write(json_encode($response_data));
+
+    return $response
+    ->withHeader('Content-type', 'application/json')
+    ->withStatus(200);  
+});
+
+/**
  * Comprueba si un par Usuario/Contraseña existe en la base de datos
  */
 $app->post('/login', function(Request $request, Response $response){
