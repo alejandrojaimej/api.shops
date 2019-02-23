79008fcb1a40e06f13babf7e3420f96252d0241a
alejandrojaimej <alejandro@mk1.es>
2019-02-23 12:41:57 +0100

    probando otra ruta

--- models/Users.model.php
+++ models/Users.model.php
@@ -121,7 +121,7 @@ class Users extends Model{
     if($userId === false || empty($userId) || $image === false || empty($image)){return false;}
     
     //comprobar que las rutas de subida existen y tienen permisos
-    $path = "../../admin.mk1/public/assets/images/user/$userId/";
+    $path = dirname($_SERVER['DOCUMENT_ROOT'])."/../../admin.mk1/public/assets/images/user/$userId/";
     if (!is_dir($path)) {
       mkdir($path);
       chmod($path, 0777);
