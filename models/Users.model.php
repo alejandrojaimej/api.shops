f57dc88532f58d9fc83c87a5e27d7d1bbbea6bb2
alejandrojaimej <alejandro@mk1.es>
2019-02-23 12:34:59 +0100

    VUELTA A LA RUTA ABSOLUTA

--- models/Users.model.php
+++ models/Users.model.php
@@ -121,7 +121,7 @@ class Users extends Model{
     if($userId === false || empty($userId) || $image === false || empty($image)){return false;}
     
     //comprobar que las rutas de subida existen y tienen permisos
-    $path = "var/www/html/admin.mk1/public/assets/images/user/$userId/";
+    $path = "/var/www/html/admin.mk1/public/assets/images/user/$userId/";
     if (!is_dir($path)) {
       mkdir($path);
       chmod($path, 0777);
@@ -134,7 +134,7 @@ class Users extends Model{
 
     $tempFile = $image['tmp_name'];
     $targetFile =  $path. $image['name'];
-    $res = copy($tempFile,$targetFile);
+    $res = move_uploaded_file($tempFile,$targetFile);
     return $res;
   }
 }
