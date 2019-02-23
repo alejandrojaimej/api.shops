f803e3548deed1dcdff3e6deb1350ac99716e141
alejandrojaimej <alejandro@mk1.es>
2019-02-23 11:39:21 +0100

    actualizacion de rutas y permisos

--- models/Users.model.php
+++ models/Users.model.php
@@ -120,8 +120,19 @@ class Users extends Model{
   public static function uploadImage($userId = false, $image = false){
     if($userId === false || empty($userId) || $image === false || empty($image)){return false;}
     
+    //comprobar que las rutas de subida existen y tienen permisos
+    if (!is_dir("/var/www/html/admin.mk1/public/images/user/$userId")) {
+      mkdir("/var/www/html/admin.mk1/public/images/user/$userId");
+      chmod("/var/www/html/admin.mk1/public/images/user/$userId", 0777);
+    }
+    $path = "/var/www/html/admin.mk1/public/images/user/$userId/gallery/";
+    if (!is_dir($path)) {
+        mkdir($path);
+        chmod($path, 0777);
+    }
+
     $tempFile = $image['file']['tmp_name'];
-    $targetFile =  BASEDIR."/../public/images/user/$userId/gallery/". $image['file']['name'];
+    $targetFile =  $path. $image['file']['name'];
     move_uploaded_file($tempFile,$targetFile);
     return  $targetFile;
   }
